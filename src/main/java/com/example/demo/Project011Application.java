485312df942e6e3fc98e419fef679a887fa9f7a4
Son Seung Gyu <thstmd0705@naver.com>
2024-02-06 13:44:25 +0900

    rest API

--- src/main/java/com/example/demo/Project011Application.java
+++ src/main/java/com/example/demo/Project011Application.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @SpringBootApplication
 public class Project011Application {
 
